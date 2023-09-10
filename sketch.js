020d2169dc44b22c69a3cef8e0f93ff1eccd3562
Hayhay1987 <105040863+Hayhay1987@users.noreply.github.com>
2023-09-10 10:20:14 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,12 @@ function setup() {
   c = createButton("Hack in infinite cookies!")
   c.position(0,150)
   c.mousePressed(hi)
+  c2 = createButton("Hack in all the acheivements!")
+  c2.position(100, 150)
+  c2.mousePressed(bee)
 }
 
 function hi() {
-  original = b.value()
   d = b.value().split("%", 2)
   e = atob(d[0])
   f = e.split("|")
@@ -20,7 +22,21 @@ function hi() {
   f[4] = g.join(";")
   h = f.join("|")
   i = btoa(h)
-  j= createElement("p", i)
+  element(i)
+}
+
+function bee() {
+  d = b.value().split("%", 2)
+  e = atob(d[0])
+  f = e.split("|")
+  console.log(f)
+  //i = btoa(h)
+  //element(i)
+}
+
+function element(asdasd) {
+  j.remove()
+  j = createElement("p", asdasd)
   j.position(0, 200)
-  j.style("font-size", "1px")
+  j.style('font-size', '1px')
 }
